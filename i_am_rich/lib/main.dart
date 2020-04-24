2e9600fad9a00eada148ad0557321aba46c74b92
weiyan <im.wayne.wy@gmail.com>
2020-04-24 15:35:42 -0600

    finish part2: how to create flutter apps from scratch

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -11,7 +11,8 @@ void main() {
         backgroundColor: Colors.blueGrey,
         body: Center(
           child: Image(
-            image: NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+            image: AssetImage('images/diamond.png'),
+            // image: NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
           ),
         ),
       ),
